cf9660ab310a1da47f781c124b4c22bdd0d64b52
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2024-01-18 17:27:08 -0500

    name bugs fixed

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import '../src/assets/css/tailwind.css'
+import "../src/assets/css/materialdesignicons.min.css"
+import { BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+);
